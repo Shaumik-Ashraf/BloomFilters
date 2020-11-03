965cbc722d48679560423c81d1cdd9cf08ebe3e4
Shaumik-Ashraf <a.shaumik@gmail.com>
2020-11-03 16:57:57 -0500

    created test code for standard bloom filter; debugged; SBF works!

--- bloomfilter.py
+++ bloomfilter.py
@@ -1,99 +1,101 @@
-#bloom_filter.py
+#bloomfilter.py
 
-import abc; #abstract base class
-import hashlib;
+from abc import ABC, abstractmethod;      #abstract base class
+import hashlib;                  
 from bitstring import BitArray;  #requires pip install bitstring
 
-class AbstractBloomFilter(metaclass=abc.ABCMeta):
-	"Abstract Bloom Filter Class" #documentation
+class AbstractBloomFilter(ABC):
+    "Abstract Bloom Filter Class" #documentation
 
-	## instance variables
-	array;  	#either BitArray or Linked List
-	size;		#non-negative int
-	hash_names; #list of strings
+    ## instance variables
+    #array;       #either BitArray or Linked List
+    #size;         #non-negative int
+    #hash_names; #list of strings
 
-	## abstract methods
-	#add param to bloom filter
-	@abstractmethod
-	def add(string):
-		pass
-	
-	#returns: True or False
-	@abstractmethod
-	def has(string):
-		pass
-	
-	#remove string from bloom filter; not possible for all types of bloom filters
-	@abstractmethod
-	def remove(string):
-		pass
+    ## abstract methods
+    #add param to bloom filter
+    @abstractmethod
+    def add(string):
+        pass
+    
+    #returns: True or False
+    @abstractmethod
+    def has(string):
+        pass
+    
+    #remove string from bloom filter; not possible for all types of bloom filters
+    @abstractmethod
+    def remove(string):
+        pass
 
-	#reset bloom filter
-	@abstractmethod
-	def reset(s):
-		pass
+    #reset bloom filter
+    @abstractmethod
+    def reset(s):
+        pass
 
-	## methods
-	#param size: size of array
-	#param array: initial array, either BitArray of 0s or List of 0s
-	#param hash_names: list of strings for hashlib.new(), hash function must be supported
-	def __init__(self, size, array, hash_names = ['sha1', 'sha224', 'sha256']);
-		self.size = size;
-		self.array = array;
-		self.hash_names = hash_names;
-		self.load = None;
-		for s in hash_names():
-			if( s not in hashlib.algorithms_available() ):
-				raise Exception("Hash " + s + " not found");
-	
-	#returns: string representing bloom filter; allows you to do: print( my_bloom_filter );
-	def __str__(self):
-		if isinstance(self, BitArray):
-			return array.bin;
-		else:
-			return str( array );
-	
-	#param k: int corresponding to hash function; k=0 is first hash in hash_names
-	#param string: python string, must be convertable to 8 bit encoding
-	#returns: non-negative int within bloomfilter size
-	def hash(k, string):
-		bytestring = string.encode();
-		hash = hashlib.new(hash_names[k]);
-		digest = hash.update(bytestring);
-		return( int(digest) % self.size );
-	
-	#iterates along each bucket returned by each hash in bloom filter
-	#param string: python string, must be convertable to 8 bit encoding
-	def each_hashed_bucket_of(string):
-		"""
-		#i.e. adding 1 to each bucket for "hello"
-		for index in each_hashed_bucket_of("hello"):
-			array[index] = array[index] + 1;
-		"""
-		for k in range(len(hash_names)):
-			yield hash(k, string);
-		
+    ## methods
+    #param size: size of array
+    #param array: initial array, either BitArray of 0s or List of 0s
+    #param hash_names: list of strings for hashlib.new(), hash function must be supported
+    def __init__(self, size, array, hash_names = ['sha1', 'sha224', 'sha256']):
+        self.size = size;
+        self.array = array;
+        self.hash_names = hash_names;
+        self.load = None;
+        for s in self.hash_names:
+            if( s not in hashlib.algorithms_available ):
+                raise Exception("Hash " + s + " not found");
+    
+    #returns: string representing bloom filter; allows you to do: print( my_bloom_filter );
+    def __str__(self):
+        if isinstance(self, BitArray):
+            return self.array.bin;
+        else:
+            return str( self.array );
+    
+    #param k: int corresponding to hash function; k=0 is first hash in hash_names
+    #param string: python string, must be convertable to 8 bit encoding
+    #returns: non-negative int within bloomfilter size
+    def hash(self, k, string):
+        bytestring = string.encode();
+        h = hashlib.new(self.hash_names[k]);
+        h.update(bytestring);
+        digest = h.hexdigest();
+        #print("Hash %s on %s gets %i" % (self.hash_names[k], string, int(digest, 16) % self.size))
+        return( int(digest, 16) % self.size );
+    
+    #iterates along each bucket returned by each hash in bloom filter
+    #param string: python string, must be convertable to 8 bit encoding
+    def each_hashed_bucket_of(self, string):
+        """
+        #i.e. adding 1 to each bucket for "hello"
+        for index in self.each_hashed_bucket_of("hello"):
+            self.array[index] = self.array[index] + 1;
+        """
+        for k in range(len(self.hash_names)):
+            yield self.hash(k, string);
+        
 
 class StandardBloomFilter(AbstractBloomFilter):
 
-	def __init__(self, size):
-		super(StandardBloomFilter, self).__init__(size, BitArray(length=size));
-	
-	def add(string):
-		for i in each_hashed_bucket_of(string):
-			array.set('1', i);
-		return;
-	
-	def has(string):
-		ret = True;
-		tmp = array.bin;
-		for i in each_hashed_bucket_of(string):
-			ret = (ret and (tmp[i] == '1'));
-		return(ret);
-	
-	def remove(string):
-		raise Exception("Remove cannot be performed by Standard Bloom Filter");
+    def __init__(self, size):
+        super(StandardBloomFilter, self).__init__(size, BitArray(length=size));
+    
+    def add(self, string):
+        for i in self.each_hashed_bucket_of(string):
+            #print(i);
+            self.array.set('1', i);
+        return;
+    
+    def has(self, string):
+        ret = True;
+        tmp = self.array.bin;
+        for i in self.each_hashed_bucket_of(string):
+            ret = (ret and (tmp[i] == '1'));
+        return(ret);
+    
+    def remove(self, string):
+        raise Exception("Remove cannot be performed by Standard Bloom Filter");
 
-	def reset():
-		array.clear();
-	
+    def reset(self):
+        self.array.set(0);
---
+++ test.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  3 16:16:58 2020
+
+@author: ShaumikAshraf
+"""
+
+import bloomfilter;
+
+print("======= Testing Standard Bloom Filter =========");
+#added = [];
+sbf = bloomfilter.StandardBloomFilter(10);
+
+print("BloomFilter: %s" % str(sbf));
+
+s = "apple";
+print("\nAdding %s ..." % s);
+#added.append(s);
+sbf.add(s);
+print("BloomFilter: %s" % str(sbf));
+
+print("\nChecking %s in BF..." % s);
+print( sbf.has(s) );
+print("BloomFilter: %s" % str(sbf));
+
+s = "banana";
+print("\nChecking %s in BF..." % s);
+print( sbf.has(s) );
+print("BloomFilter: %s" % str(sbf));
+
+s = "tower";
+print("\nChecking %s in BF..." % s);
+print( sbf.has(s) );
+print("BloomFilter: %s" % str(sbf));
+
+s = "banana";
+print("\nAdding %s ..." % s);
+sbf.add(s);
+print("BloomFilter: %s" % str(sbf));
+
+s = "tower";
+print("\nChecking %s in BF..." % s);
+print( sbf.has(s) );
+print("BloomFilter: %s" % str(sbf));
+
+print("\nTrying remove, should raise exception for SBF");
+try:
+    sbf.remove(s);
+except:
+    print("Exception raised successfully")
+finally:
+    print("Exception should have been raised.");
+
+print("\nReseting BF");
+sbf.reset();
+print("BloomFilter: %s" % str(sbf));
+
+a = "apple";
+print("\nChecking %s in BF..." % s);
+print( sbf.has(s) );
+print("BloomFilter: %s" % str(sbf));
+
+print("\n===================== Done =====================");
+
